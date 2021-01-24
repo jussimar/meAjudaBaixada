55c5e7adc6f84763fd9b0ae2dbbdd1728eb7721c
anabeatrizzz <anabeatriz.augusto06@yahoo.com>
2021-01-24 10:21:00 -0300

    tabs to spaces

--- MeAjudaBaixada/categoria.php
+++ MeAjudaBaixada/categoria.php
@@ -1,27 +1,30 @@
 <?php
-	// Arquivo que fará um trio com conexao.php e o front-end.
-	
-	// Funções que fazem inserção ou atualização de alguma informação são POST. O GET será usado em consultas ou exclusões de informações.
+  // Arquivo que fará um trio com conexao.php e o front-end.
+  
+  // Funções que fazem inserção ou atualização de alguma informação são POST. O GET será usado em consultas ou exclusões de informações.
 
-	if($_POST){
-		// $_POST['acao'] seria, por exemplo, um form com name="acao" com um input com name="cadastrar"
-		if($_POST['acao'] == 'cadastrar'){
-			CadastrarCategoria($_POST['nome']);
-		}
-		else if($_POST['acao'] == 'atualizar'){
-			AtualizarCategoria($_POST['cd_categoria'], $_POST['nm_categoria']);
-		}
-	}
-	if($_GET){
-		if($_GET['acao'] == 'excluir'){
-			ExcluirCategoria($_GET['cd_categoria']);
-		}
-		else if($_GET['acao'] == 'listar'){
-			$res = ListarCategoria($_GET['cd_categoria']);
+  if($_POST){
+    // $_POST['acao'] seria, por exemplo, um form com name="acao" com um input com name="cadastrar"
+    if($_POST['acao'] == 'cadastrar'){
+      CadastrarCategoria($_POST['nome']);
+    }
+    
+    else if($_POST['acao'] == 'atualizar'){
+      AtualizarCategoria($_POST['cd_categoria'], $_POST['nm_categoria']);
+    }
+  }
 
-			// Fazendo a codificação pois esta função não retorna mensagem
-			echo json_encode($res);
-		}
-	}
+  if($_GET){
+    if($_GET['acao'] == 'excluir'){
+      ExcluirCategoria($_GET['cd_categoria']);
+    }
+    
+    else if($_GET['acao'] == 'listar'){
+      $res = ListarCategoria($_GET['cd_categoria']);
+
+      // Fazendo a codificação pois esta função não retorna mensagem
+      echo json_encode($res);
+    }
+  }
 
 ?>
--- MeAjudaBaixada/dependente.php
+++ MeAjudaBaixada/dependente.php
@@ -1,21 +1,23 @@
 <?php
-	if($_POST){
-		if($_POST['acao'] == 'cadastrar'){
-			CadastrarDependente($_POST['nm_dependente'], $_POST['dt_nascimento'], $_POST['cd_cpf'], $_POST['id_solicitante']);
-		}
-		else if($_POST['acao'] == 'atualizar'){
-			AtualizarDependente($_POST['cd_dependente'], $_POST['nm_dependente'], $_POST['dt_nascimento'], $_POST['cd_cpf'], $_POST['id_solicitante']);
-		}
-	}
+  if($_POST){
+    if($_POST['acao'] == 'cadastrar'){
+      CadastrarDependente($_POST['nm_dependente'], $_POST['dt_nascimento'], $_POST['cd_cpf'], $_POST['id_solicitante']);
+    }
+    
+    else if($_POST['acao'] == 'atualizar'){
+      AtualizarDependente($_POST['cd_dependente'], $_POST['nm_dependente'], $_POST['dt_nascimento'], $_POST['cd_cpf'], $_POST['id_solicitante']);
+    }
+  }
 
-	if($_GET){
-		if($_GET['acao'] == 'excluir'){
-			ExcluirDependente($_POST['cd_dependente']);
-		}
-		else if($_GET['acao'] == 'listar'){
-			$res = ListarDependente($_POST['cd_dependente'], $_POST['id_solicitante']);
+  if($_GET){
+    if($_GET['acao'] == 'excluir'){
+      ExcluirDependente($_POST['cd_dependente']);
+    }
+    
+    else if($_GET['acao'] == 'listar'){
+      $res = ListarDependente($_POST['cd_dependente'], $_POST['id_solicitante']);
 
-			echo json_encode($res);
-		}
-	}
+      echo json_encode($res);
+    }
+  }
 ?>
